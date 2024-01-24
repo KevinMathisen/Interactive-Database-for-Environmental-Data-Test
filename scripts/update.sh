59b4e05e9ef7bf3c3ae88acdb08b869086adfe1a
Kevin Nikolai Mathisen <51268187+KevinMathisen@users.noreply.github.com>
2024-01-24 13:35:31 +0100

    ops: remove comments

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,4 @@ cd /home/ubuntu/Interactive-Database-for-Environmental-Data-Test
 # Pull the latest changes from the GitHub repository
 git pull origin main
 
-#chmod +x ./scripts/update.sh
-# -
 # Restart your web server or perform any other necessary actions
