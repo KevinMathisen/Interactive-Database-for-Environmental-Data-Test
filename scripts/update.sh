eeebef9038dd3e2ee7ef09f53721b9d8b45dccb3
Kevin Nikolai Mathisen <51268187+KevinMathisen@users.noreply.github.com>
2024-01-24 13:14:51 +0100

    ops: remove deploy filler code

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,6 +6,6 @@ cd ..
 # Pull the latest changes from the GitHub repository
 git pull origin main
 
+#chmod +x ./scripts/update.sh
+
 # Restart your web server or perform any other necessary actions
-# For example, if using Nginx:
-sudo systemctl restart nginx
