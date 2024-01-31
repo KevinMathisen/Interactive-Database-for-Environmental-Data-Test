c20ec3feb7d553985722f961d9aa573e2146ce79
carlpm <carlpettermr@gmail.com>
2024-01-31 14:57:23 +0000

    Apply lint fixes

--- web-server/dummy.js
+++ web-server/dummy.js
@@ -1,8 +1,8 @@
 // Example of code with linting issues
-function calculateSum(num1, num2) {
-  var result = num1 + num2;
-  return result;
+function calculateSum (num1, num2) {
+  const result = num1 + num2
+  return result
 }
 
-var total = calculateSum(5, 10);
-console.log("Total is: " + total);
+const total = calculateSum(5, 10)
+console.log('Total is: ' + total)
