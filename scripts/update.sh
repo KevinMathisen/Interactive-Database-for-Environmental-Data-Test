b6f9c572690a2698ae378640c869566d15e4abaa
bowcarl <35300302+bowcarl@users.noreply.github.com>
2024-02-19 14:54:19 +0100

    039-build: update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail  # Exists if any command fail
+
 # Navigate to the website directory
 cd /home/ubuntu/Interactive-Database-for-Environmental-Data-Test
 
@@ -7,3 +9,15 @@ cd /home/ubuntu/Interactive-Database-for-Environmental-Data-Test
 git pull origin main
 
 # Restart your web server or perform any other necessary actions
+
+# Stops and remove the container if it exists, ignoring errors if it's not running
+docker stop staticwebserver:latest || true
+docker rm staticwebserver:latest || true
+
+# Build the Docker image
+docker build -t "staticwebserver:latest" ./web-server
+
+# Run the Docker container
+docker run --restart=always -p 3000:3000 -d staticwebserver:latest
+
+echo "Static web server started successfully."
